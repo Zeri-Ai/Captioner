c54037d896205e6c9bd185dbd112dfb98677d444
Zeri-Ai <86636498+Zeri-Ai@users.noreply.github.com>
2022-03-25 20:19:07 -0500

    UserInterface barebones dayo

---
+++ src/main/java/UI/UserInterface.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author HCastilloR
+ */
+public class UserInterface {
+
+    public static final Dimension DEFAULT_DIMENSIONS;
+    public static final Dimension CANVAS_DEFAULT_DIMENSIONS;
+    private final JFrame frame;
+    private final Canvas canvas;
+
+    static {
+        DEFAULT_DIMENSIONS = new Dimension(800, 600);
+        CANVAS_DEFAULT_DIMENSIONS = new Dimension(DEFAULT_DIMENSIONS.width, DEFAULT_DIMENSIONS.height / 6 * 5);
+    }
+
+    private class Canvas extends JPanel {
+
+        private BufferedImage bf;
+
+        @Override
+        public final void setDoubleBuffered(boolean aFlag) {
+            super.setDoubleBuffered(aFlag); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        public void setImage(BufferedImage bf) {
+            this.bf = bf;
+        }
+
+        public Canvas() {
+            super();
+            setDoubleBuffered(true);
+            bf = null;
+        }
+
+    }
+
+    public void setImage(BufferedImage bf) {
+        canvas.setImage(bf);
+    }
+
+    public UserInterface() {
+        frame = new JFrame("Captioneer");
+        canvas = new Canvas();
+    }
+}
